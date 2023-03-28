2b79caaac9e22b7f77b856ebecfc619bea4d4bc9
BeanyBB <ben.blankenbiller@gmail.com>
2023-03-28 00:02:53 -0500

    Added pygame functionalityf for two circles of the same object class to move left and right using different keys

--- main.py
+++ main.py
@@ -1,13 +1,55 @@
 from player import Player
+import pygame
+
+def game_loop(screen, clock, running, dt):
+    player1 = Player('sword', 'default', 'player1', screen,
+                     pygame.Vector2(screen.get_width() / 1.5, screen.get_height() / 2), "red")
+    player2 = Player('sword', 'default', 'player1', screen,
+                     pygame.Vector2(screen.get_width() / 3, screen.get_height() / 2), "blue")
+
+    while running:
+
+        # poll for events
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("purple")
+
+        player1.show_player()
+        player2.show_player()
+
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            player1.move_left(dt)
+        if keys[pygame.K_d]:
+            player1.move_right(dt)
+        if keys[pygame.K_RIGHT]:
+            player2.move_right(dt)
+        if keys[pygame.K_LEFT]:
+            player2.move_left(dt)
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        # dt is delta time in seconds since last frame, used for framerate-
+        # independent physics.
+        dt = clock.tick(60) / 1000
+
+    pygame.quit()
 
 def main():
-    player1 = Player('sword', 'default', 'player1')
-    player2 = Player('sword', 'default', 'player2')
-    player1.attack()
-    player2.attack()
-    player2.defend()
-    player2.defend()
-    player1.special_attack()
+    pygame.init()
+    screen = pygame.display.set_mode((1280, 720))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+
+    game_loop(screen, clock, running, dt)
+
 
 
 if __name__ == '__main__':
--- player.py
+++ player.py
@@ -1,9 +1,16 @@
+import pygame
 
 class Player:
-    def __init__(self, weapon, avatar, name):
+    def __init__(self, weapon, avatar, name, screen, player_pos, color):
+        self.color = color
+        self.player_pos = player_pos
+        self.screen = screen
         self.weapon = weapon
         self.avatar = avatar
         self.name = name
+
+    def show_player(self):
+        pygame.draw.circle(self.screen, self.color, self.player_pos, 40)
 
     def attack(self):
         print(f'{self.name} attacking')
@@ -14,11 +21,15 @@ class Player:
     def jump(self):
         print(f'{self.name} jumping')
 
-    def move_left(self):
+    def move_left(self, dt):
+        self.player_pos.x -= 300 * dt
         print(f'{self.name} moving left')
 
-    def move_right(self):
+    def move_right(self, dt):
+        print(self.player_pos)
+        self.player_pos.x += 300 * dt
         print(f'{self.name} moving right')
+        print(self.player_pos)
 
     def special_attack(self):
         print(f'{self.name} doing special attack')
